552d0d393bb3b162a0edc562867a784dcf2c39e3
Leonard Huang <leihuang.org@gmail.com>
2017-05-25 21:50:49 +0800

    Update login.sh

--- scripts/login.sh
+++ scripts/login.sh
@@ -6,15 +6,17 @@ username=$(echap "$username")
 password=$(echap "$password")
 
 # Authentication
-user_pass=$(mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -sN -e "SELECT user_pass FROM user WHERE user_id = '$username' AND user_enable=1 AND (TO_DAYS(now()) >= TO_DAYS(user_start_date) OR user_start_date IS NULL) AND (TO_DAYS(now()) <= TO_DAYS(user_end_date) OR user_end_date IS NULL)")
+# password_hash=$(mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -sN -e "SELECT password_hash FROM user WHERE user_id = '$username' AND user_enable=1 AND (TO_DAYS(now()) >= TO_DAYS(user_start_date) OR user_start_date IS NULL) AND (TO_DAYS(now()) <= TO_DAYS(user_end_date) OR user_end_date IS NULL)")
+password_hash=$(mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -sN -e "SELECT password_hash FROM user WHERE email = '$username' AND enable=1")
 
 # Check the user
-if [ "$user_pass" == '' ]; then
+if [ "$password_hash" == '' ]; then
   echo "$username: bad account."
   exit 1
 fi
 
-result=$(php -r "if(password_verify('$password', '$user_pass') == true) { echo 'ok'; } else { echo 'ko'; }")
+# result=$(php -r "if(password_verify('$password', '$user_pass') == true) { echo 'ok'; } else { echo 'ko'; }")
+result=$(python -c "from werkzeug.security import check_password_hash if(check_password_hash('$password', '$password_hash') == true) { echo 'ok'; } else { echo 'ko'; }")
 
 if [ "$result" == "ok" ]; then
   echo "$username: authentication ok."
